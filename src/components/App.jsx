d604356675de82b5d4b3677dc6a185a3543c9b36
Durga Sumesh <durgasumesh17@gmail.com>
2024-09-18 14:08:31 +0530

    add elements of counter

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App(){
+    return <div className = "counter">
+        <h1>Count</h1>
+        <button>-</button>
+        <button>+</button>
+    </div>
+}
+
+export default App;
