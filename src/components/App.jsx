1ed044b13e7d9c1a5896f8e014ec20c322d48849
Durga Sumesh <durgasumesh17@gmail.com>
2024-09-18 14:13:35 +0530

    add functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 
 function App(){
+    const [count, editCount] = React.useState(0);
+
+    function add(){
+        editCount(count + 1);
+    }
+
+    function subtract(){
+        editCount(count - 1);
+    }
     return <div className = "counter">
-        <h1>Count</h1>
-        <button>-</button>
-        <button>+</button>
+        <h1>{count}</h1>
+        <button onClick = {subtract}>-</button>
+        <button onClick = {add}>+</button>
     </div>
 }
 
